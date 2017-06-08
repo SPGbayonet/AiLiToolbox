9e6ab8531caf6b0abf4ef250e89864a6cbc1e3da
红花老魔 <1400852942@qq.com>
2017-06-08 09:23:50 +0800

    修改测试用例

--- Example/Tests/TSDate/TestsDateWeek.swift
+++ Example/Tests/TSDate/TestsDateWeek.swift
@@ -14,14 +14,14 @@ class TestsDateWeek: TestsDate {
 
     override func spec() {
         resetTimes()
-        describe("传入一天之内的时间") {
+        describe("传入五个不同的值") {
 
             let oneString = TSDate(now: egday).dateString(.wallet, nsDate: today as NSDate)
             let twoString = TSDate(now: egday).dateString(.wallet, nsDate: Yesterday as NSDate)
             let threeString = TSDate(now: egday).dateString(.wallet, nsDate: toyesterday as NSDate)
             let fourString = TSDate(now: egday).dateString(.wallet, nsDate: otherday as NSDate)
             let fiveString = TSDate(now: egday).dateString(.walletdetail, nsDate: egday as NSDate)
-            it("将时间戳转换成显示字符串") {
+            it("进行输出比较") {
                 let today = "今天\n06.07"
                 let yesterday = "昨天\n06.06"
                 let nineDayAnswer = "前天\n06.05"
